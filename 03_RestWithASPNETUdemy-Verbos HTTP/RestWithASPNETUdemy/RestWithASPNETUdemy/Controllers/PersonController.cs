eb405fbedf657eefec2e1d53d1f0fba1bae637ab
Heverton Reis <heverton.reis04@gmail.com>
2020-12-29 11:30:38 -0300

    Trabalhando com Verbos HTTP

---
+++ 03_RestWithASPNETUdemy-Verbos HTTP/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var operation = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                    return Ok(operation.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }        
+    }
+}
