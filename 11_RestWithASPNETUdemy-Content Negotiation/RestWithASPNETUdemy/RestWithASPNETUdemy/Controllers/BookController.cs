599b5167b55b15e46d7e00ba859ece27d89f7a54
Heverton Reis <heverton.reis04@gmail.com>
2021-01-11 14:43:51 -0300

    Adicionando suporte a XML

---
+++ 11_RestWithASPNETUdemy-Content Negotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BookController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    
+    public class BookController : ControllerBase
+    {
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+       
+        // GET: api/Books
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindByID(id);
+            if (book == null) NotFound();
+            return Ok(book);
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        // PUT: api/Books/5
+        [HttpPut]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+            if(book == null) return BadRequest();
+            return Ok(_bookBusiness.Update(book));
+        }    
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
