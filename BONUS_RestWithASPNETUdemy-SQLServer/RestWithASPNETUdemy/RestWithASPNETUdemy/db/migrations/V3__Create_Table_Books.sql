08c08540ef68fab1599db722483707885b3c62ad
Heverton Reis <heverton.reis04@gmail.com>
2021-02-01 09:45:53 -0300

    Conectando ao BD SQL Server

---
+++ BONUS_RestWithASPNETUdemy-SQLServer/RestWithASPNETUdemy/RestWithASPNETUdemy/db/migrations/V3__Create_Table_Books.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.books (
+  [id] bigint NOT NULL IDENTITY,
+  [author] varchar(max),
+  [launch_date] datetime2(6) NOT NULL,
+  [price] decimal(38,2) NOT NULL,
+  [title] varchar(max),
+  PRIMARY KEY ([id])
+)
+GO
---
+++ BONUS_RestWithASPNETUdemy-SQLServer/RestWithASPNETUdemy/RestWithASPNETUdemy/db/migrations/V5__Create_Table_Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.users (
+  [id] bigint NOT NULL IDENTITY,
+  [user_name] varchar(50) NOT NULL DEFAULT '0',
+  [password] varchar(130) NOT NULL DEFAULT '0',
+  [full_name] varchar(120) NOT NULL,
+  [refresh_token] varchar(500) DEFAULT '0',
+  [refresh_token_expiry_time] datetime2(0) DEFAULT NULL,
+  PRIMARY KEY ([id]),
+  CONSTRAINT [user_name] UNIQUE  ([user_name])
+)
+GO
